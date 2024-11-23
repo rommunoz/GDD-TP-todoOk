d7e39e8567b085ea9071557dbb64f2877cf0d3d4
abrilnimo <adomingueznimo@frba.utn.edu.ar>
2024-11-23 20:59:20 -0300

    cosas

--- script_creacion_BI.sql
+++ script_creacion_BI.sql
@@ -78,8 +78,14 @@ ALTER TABLE todoOk.BI_Hechos_Envios ADD CONSTRAINT FK_BI_Dimension_Tiempo FOREIG
 ALTER TABLE todoOk.BI_Hechos_Envios ADD CONSTRAINT FK_BI_Dimension_Rango_Etario FOREIGN KEY (d_rango_etario_id) REFERENCES BI_Dimension_Rango_Etario(d_rango_etario_id)
 ----
 
+CREATE TABLE todoOk.BI_Hechos_Factura(
+    h_factura_
+EGRETNI d
+)
+
 CREATE TABLE todoOk.BI_Hechos_Ventas(
-    ubicacion_id           INTEGER IDENTITY(1,1),
+    h_venta_id             INTEGER IDENTITY(1,1),
+    ubicacion_id           INTEGER,
     rango_horario_id       INTEGER,
     rubro_id               INTEGER,
     rango_etario_id        INTEGER,
@@ -89,7 +95,10 @@ CREATE TABLE todoOk.BI_Hechos_Ventas(
     venta_rubro            INTEGER,       
     cant_ventas_horario    INTEGER
 )
-
+-- PK
+ALTER TABLE todoOk.BI_Hechos_Ventas ADD CONSTRAINT PK_BI_Hechos_Ventas PRIMARY KEY (h_venta_id)
+-- FK 
+ALTER TABLE todoOk.BI_Hecho_Ventas ADD CON
 
 CREATE TABLE todoOk.BI_Hechos_Publicacion(
     h_publicacion_id        INTEGER IDENTITY(1,1),
@@ -99,7 +108,6 @@ CREATE TABLE todoOk.BI_Hechos_Publicacion(
     fecha_inicio            DATE,
     fecha_fin               DATE,
     stock                   DECIMAL
-
 )
 
 ---------CONSTRAINTS PRIMARIAS----------------------
@@ -135,3 +143,84 @@ ADD CONSTRAINT PK_BI_Dimension_Publicacion PRIMARY KEY (d_publicacion_id);
 
 ALTER TABLE todoOk.BI_Dimension_Tipo_Envio 
 ADD CONSTRAINT PK_BI_Dimension_Tipo_Envio PRIMARY KEY (d_tipo_envio_id);
+
+
+------------CONSTRAINTS FORANEAS----------------
+
+------------MIGRACIONES------------------------
+------------TABLAS DE DDIMENSIONES----------
+
+CREATE PROCEDURE todoOk.BI_Migrar_Dimension_Rango_Etario
+AS
+BEGIN
+    INSERT INTO BI_Dimension_Rango_Etario()
+    VALUES ('<25')
+    INSERT INTO BI_Dimension_Rango_Etario(descripcion)
+    VALUES ('25-35')
+    INSERT INTO BI_Dimension_Rango_Etario(descripcion)
+    VALUES ('35-50')
+    INSERT INTO BI_Dimension_Rango_Etario(descripcion)
+    VALUES ('>50')
+    INSERT INTO BI_Dimension_Rango_Etario(descripcion)
+    VALUES ('FUERA_DE_RANGO')
+END
+GO
+
+CREATE PROCEDURE todoOk.BI_Migrar_Dimension_Rango_Horario
+AS
+BEGIN
+    INSERT INTO BI_Dimension_Rango_Horario(descripcion)
+    VALUES ('00:00 - 06:00')
+    INSERT INTO BI_Dimension_Rango_Horario(descripcion)
+    VALUES ('06:00 - 12:00')
+    INSERT INTO BI_Dimension_Rango_Horario(descripcion)
+    VALUES ('12:00 - 18:00')
+    INSERT INTO BI_Dimension_Rango_Horario(descripcion)
+    VALUES ('18:00 - 24:00')
+END
+GO
+
+-- Migración Ubicacion
+
+CREATE PROCEDURE todoOk.BI_MIGRAR_D_UBICACION
+AS
+BEGIN
+    INSERT INTO BI_D_UBICACION(UBI_PROVINCIA, UBI_LOCALIDAD)
+        (SELECT prov_nombre, localidad_nombre
+         FROM todoOk.Localidad
+                  JOIN todoOk.Provincia P ON P.prov_cod = Localidad.localidad_prov)
+END
+GO
+
+-- Migración Tiempo
+
+CREATE PROCEDURE todoOk.BI_MIGRAR_D_TIEMPO
+AS
+BEGIN
+    -- noinspection SqlShouldBeInGroupBy
+    INSERT INTO BI_D_TIEMPO(TIEMPO_ANIO, TIEMPO_CUATRIMESTRE, TIEMPO_MES)
+        (SELECT YEAR(ticket_fecha), DATEPART(QUARTER, ticket_fecha), MONTH(ticket_fecha)
+         FROM todoOk.Ticket
+         GROUP BY YEAR(ticket_fecha), DATEPART(QUARTER, ticket_fecha), MONTH(ticket_fecha)
+         UNION
+         SELECT YEAR(pago_fecha), DATEPART(QUARTER, pago_fecha), MONTH(pago_fecha)
+         FROM todoOk.Pago
+         GROUP BY YEAR(pago_fecha), DATEPART(QUARTER, pago_fecha), MONTH(pago_fecha)
+         UNION
+         SELECT YEAR(prog_env_fecha_programacion),
+                DATEPART(QUARTER, prog_env_fecha_programacion),
+                MONTH(prog_env_fecha_programacion)
+         FROM todoOk.Programacion_Envio
+         GROUP BY YEAR(prog_env_fecha_programacion), DATEPART(QUARTER, prog_env_fecha_programacion),
+                  MONTH(prog_env_fecha_programacion))
+END
+GO
+
+-- Migracion de medio pago
+CREATE PROCEDURE todoOk.BI_MIGRAR_D_MEDIO_PAGO
+AS
+BEGIN
+    INSERT INTO BI_D_MEDIO_PAGO(MP_DETALLE, MP_TIPO)
+        (select MP_detalle, MP_TIPO from todoOk.Medio_pago)
+END
+GO
